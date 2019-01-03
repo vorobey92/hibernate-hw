161f346579ebb9cbb065a10add4800ffe1ddf406
Fedor Khodkov <fkhodkov@gmail.com>
2019-01-03 15:08:23 +0300

    Basic Operations

--- src/main/java/ru/hh/school/users/User.java
+++ src/main/java/ru/hh/school/users/User.java
@@ -1,17 +1,29 @@
 package ru.hh.school.users;
 
-/**
- * ToDo оформите entity
- *
- * @see scripts/create_users.sql
- *
- * https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#entity
- */
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User {
 
+  @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @Column(name="user_id")
   private Integer id;
+
+  @Column(name="first_name")
   private String firstName;
+
+  @Column(name="last_name")
   private String lastName;
+
+  User() {}
 
   public User(String firstName, String lastName) {
     this.firstName = firstName;
@@ -22,8 +34,33 @@ public class User {
     return id;
   }
 
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
   public void setFirstName(String firstName) {
     this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+    return firstName.equals(user.firstName) && lastName.equals(user.lastName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, lastName);
   }
 
 }
