57ecdf5197be594bc7ccf1394148b650e65cb006
Fedor Khodkov <fkhodkov@gmail.com>
2019-01-11 21:26:41 +0300

    Added constraints where it's possible

--- src/main/resources/scripts/create_user_inheritance_joined.sql
+++ src/main/resources/scripts/create_user_inheritance_joined.sql
@@ -1,15 +1,15 @@
 
 create table user_inheritance_joined (
   id serial primary key,
-  name varchar(124)
+  name varchar(124) not null
 );
 
 create table employer_inheritance_joined (
   id serial primary key,
-  company varchar(124)
+  company varchar(124) not null
 );
 
 create table applicant_inheritance_joined (
   id serial primary key,
-  position varchar(124)
+  position varchar(124) not null
 );
--- src/main/resources/scripts/create_user_inheritance_single.sql
+++ src/main/resources/scripts/create_user_inheritance_single.sql
@@ -1,7 +1,7 @@
 create table user_inheritance_single (
   DTYPE varchar(31) not null,
   id serial primary key,
-  name varchar(124),
+  name varchar(124) not null,
   position varchar(124),
   company varchar(124)
 );
