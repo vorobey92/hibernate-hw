fdf4b61ac3fa7b5264c8063193c6998e2ef5c1e8
Sergey Samofalov <samof.sergey@gmail.com>
2019-01-17 00:19:44 +0300

    07. SingleTableTest

--- src/main/resources/scripts/create_user_inheritance_single.sql
+++ src/main/resources/scripts/create_user_inheritance_single.sql
@@ -1,2 +1,7 @@
 create table user_inheritance_single (
+  id serial primary key,
+  name varchar(255) not null, -- общее для всех, может быть not null
+  company varchar(255),
+  position varchar(255),
+  dtype varchar(255) not null -- используется по умолчанию, если не определен Discriminator
 );
