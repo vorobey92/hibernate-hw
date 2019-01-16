8349bb68892917b7d0172acb3c39202987df63f8
Sergey Samofalov <samof.sergey@gmail.com>
2019-01-17 00:38:20 +0300

    09. TableStrategyTest

--- src/main/resources/scripts/create_user_inheritance_table.sql
+++ src/main/resources/scripts/create_user_inheritance_table.sql
@@ -1,10 +1,18 @@
 create sequence user_id_seq;
 
 create table user_inheritance_table (
+  id int not null default nextval('user_id_seq') primary key, -- используем общий sequence для всех наследников
+  name varchar(255) not null
 );
 
 create table employer_inheritance_table (
+  id int not null default nextval('user_id_seq') primary key,
+  name varchar(255) not null,
+  company varchar(255) not null
 );
 
 create table applicant_inheritance_table (
+ id int not null default nextval('user_id_seq') primary key,
+ name varchar(255) not null,
+ position varchar(255) not null
 );
