adf99571c146a2987c9890a2efa15c1668aabd3e
Sergey Samofalov <samof.sergey@gmail.com>
2019-01-16 23:20:51 +0300

    01. Basic Operations

--- src/main/java/ru/hh/school/users/User.java
+++ src/main/java/ru/hh/school/users/User.java
@@ -1,5 +1,8 @@
 package ru.hh.school.users;
 
+import javax.persistence.*;
+import java.util.Objects;
+
 /**
  * ToDo оформите entity
  *
@@ -7,11 +10,21 @@ package ru.hh.school.users;
  *
  * https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#entity
  */
+@Entity
+@Table(name = "users")
 public class User {
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
   private Integer id;
+
+  @Column(name = "first_name")
   private String firstName;
+
+  @Column(name = "last_name")
   private String lastName;
+
+  User() {}
 
   public User(String firstName, String lastName) {
     this.firstName = firstName;
@@ -22,8 +35,33 @@ public class User {
     return id;
   }
 
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    User user = (User) o;
+    return Objects.equals(firstName, user.firstName) &&
+        Objects.equals(lastName, user.lastName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, lastName);
+  }
 }
