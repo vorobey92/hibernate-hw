4436c5e24ca1ca94d2c2d02f979564e221dc597c
Aleksandr Fursov <apokalipto57@yandex.ru>
2019-01-26 23:57:32 +0300

    DtoMappingTest

--- src/test/java/ru/hh/school/employers/DtoMappingTest.java
+++ src/test/java/ru/hh/school/employers/DtoMappingTest.java
@@ -3,19 +3,18 @@ package ru.hh.school.employers;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.Ignore;
+import org.hibernate.transform.Transformers;
 import org.junit.Test;
 
 public class DtoMappingTest extends EmployerTest {
 
-  @Ignore
   @Test
   public void mapQueryResultToDto() {
     StatisticsDto dto = doInTransaction(() ->
-      // ToDo дополните запрос, чтобы возвращался ru.hh.school.employers.StatisticsDto
-      // https://vladmihalcea.com/the-best-way-to-map-a-projection-query-to-a-dto-with-jpa-and-hibernate/
       getSession().createQuery(
-        "select count(e.id), sum(e.bonusPoints) from Employer e", StatisticsDto.class)
+        "select new " +
+        "ru.hh.school.employers.StatisticsDto( " +
+        "count(e.id), sum(e.bonusPoints)) from Employer e", StatisticsDto.class)
         .getSingleResult()
     );
 
