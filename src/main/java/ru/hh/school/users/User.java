4028511d2c2a31331438968285483cea4525cfc3
Alexey Gorlenko <agorlenko1@gmail.com>
2019-01-29 01:08:23 +0300

    Исправлено форматирование

--- src/main/java/ru/hh/school/users/User.java
+++ src/main/java/ru/hh/school/users/User.java
@@ -21,7 +21,8 @@ public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "user_id")  private Integer id;
+  @Column(name = "user_id")
+  private Integer id;
 
   @Column(name = "first_name") 
   private String firstName;
