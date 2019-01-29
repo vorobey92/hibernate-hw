4a013f8b42c4a90973f694f741153f6d86228226
Petr Eremeykin <eremeykin@gmail.com>
2019-01-29 12:37:01 +0300

    Fix second test: NPlusOneTest

--- src/test/java/ru/hh/school/employers/NPlusOneTest.java
+++ src/test/java/ru/hh/school/employers/NPlusOneTest.java
@@ -1,24 +1,27 @@
 package ru.hh.school.employers;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Rule;
 import org.junit.Test;
 import ru.hh.school.TransactionRule;
+
 import java.util.List;
 
 public class NPlusOneTest extends EmployerTest {
+
+  private static final String QUERY_EMPLOYERS = "from Employer as emp left join fetch emp.vacancies";
 
   @Rule
   public TransactionRule transactionRule = new TransactionRule(sessionFactory);
 
   /**
    * ToDo Попробуйте модифицировать запрос так, чтобы выполнился 1 запрос
-   *
    */
   @Test
   public void shouldExecuteOneStatement() {
-    List<Employer> employers = getSession().createQuery("from Employer", Employer.class)
-      .list();
+    List<Employer> employers = getSession().createQuery(QUERY_EMPLOYERS, Employer.class)
+        .list();
 
     employers.forEach((emp) -> emp.getVacancies().size());
 
