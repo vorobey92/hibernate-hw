f29450eccd06385be0b07adfc2e1fb2458ee6ecd
Andrey Nikishin <polarik@ya.ru>
2019-01-30 15:52:36 +0300

    Complete JoinedTest

--- src/main/resources/scripts/create_user_inheritance_joined.sql
+++ src/main/resources/scripts/create_user_inheritance_joined.sql
@@ -1,9 +1,15 @@
 
 create table user_inheritance_joined (
+  id SERIAL PRIMARY KEY,
+  name varchar(124)
 );
 
 create table employer_inheritance_joined (
+  id INTEGER NOT NULL PRIMARY KEY REFERENCES user_inheritance_joined,
+  company VARCHAR(124)
 );
 
 create table applicant_inheritance_joined (
+  id INTEGER NOT NULL PRIMARY KEY REFERENCES user_inheritance_joined,
+  position VARCHAR(124)
 );
