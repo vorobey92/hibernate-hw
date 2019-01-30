b19adea7aa60b4f6b5716b025945605378158ed0
Andrey Nikishin <polarik@ya.ru>
2019-01-30 16:01:57 +0300

    Complete TableStrategyTest

--- src/main/resources/scripts/create_user_inheritance_table.sql
+++ src/main/resources/scripts/create_user_inheritance_table.sql
@@ -1,10 +1,18 @@
 create sequence user_id_seq;
 
 create table user_inheritance_table (
+  id INTEGER DEFAULT nextval('user_id_seq') NOT NULL,
+  name varchar(124)
 );
 
 create table employer_inheritance_table (
+  id INTEGER DEFAULT nextval('user_id_seq') NOT NULL,
+  name varchar(124),
+  company VARCHAR(124)
 );
 
 create table applicant_inheritance_table (
+  id INTEGER DEFAULT nextval('user_id_seq') NOT NULL,
+  name varchar(124),
+  position VARCHAR(124)
 );
--- src/test/java/ru/hh/school/inheritance/TableStrategyTest.java
+++ src/test/java/ru/hh/school/inheritance/TableStrategyTest.java
@@ -32,11 +32,6 @@ public class TableStrategyTest extends BaseTest {
     TestHelper.execute(pg.getPostgresDatabase(), "delete from applicant_inheritance_table");
   }
 
-  /**
-   * ToDo создайте нужные таблицы в файле scripts/create_user_inheritance_table.sql
-   *
-   * https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#entity-inheritance-table-per-class
-   */
   @Test
   public void saveDifferentTypes() {
     User user = new User();
