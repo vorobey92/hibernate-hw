4dc3a9c4eeafac9ea24e7a1eec6ab57fd7f42afc
Vitali Baranov <vitali.baranov@gmail.com>
2019-02-02 00:48:10 +0300

    8. JoinedTest

--- src/main/resources/scripts/create_user_inheritance_joined.sql
+++ src/main/resources/scripts/create_user_inheritance_joined.sql
@@ -1,9 +1,16 @@
 
 create table user_inheritance_joined (
+    id serial primary key,
+    name varchar(50) not null
+
 );
 
 create table employer_inheritance_joined (
+    id int primary key references user_inheritance_joined(id),
+    company varchar(50)
 );
 
 create table applicant_inheritance_joined (
+    id int primary key references user_inheritance_joined(id),
+    position varchar(50)
 );
