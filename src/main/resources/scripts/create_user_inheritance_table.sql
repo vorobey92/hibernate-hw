22b456c3a248b35e457e60b43cb781165082bb6c
Petr Eremeykin <eremeykin@gmail.com>
2019-02-01 18:14:21 +0300

    Fix ninth test: TableStrategyTest

--- src/main/resources/scripts/create_user_inheritance_table.sql
+++ src/main/resources/scripts/create_user_inheritance_table.sql
@@ -1,10 +1,19 @@
 create sequence user_id_seq;
 
 create table user_inheritance_table (
+  id serial primary key,
+  name varchar(100)
 );
 
 create table employer_inheritance_table (
+  id serial primary key,
+  name varchar(100),
+  company varchar(100)
 );
 
 create table applicant_inheritance_table (
+  id serial primary key,
+  name varchar(100),
+  company varchar(100),
+  position varchar(100)
 );
