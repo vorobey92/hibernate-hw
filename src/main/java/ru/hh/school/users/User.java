a1f596a3f0875f2019cb1bd689c917b3533f3372
Vitali Baranov <vitali.baranov@gmail.com>
2019-02-01 21:49:35 +0300

    1. Basic Operations

--- src/main/java/ru/hh/school/users/User.java
+++ src/main/java/ru/hh/school/users/User.java
@@ -1,5 +1,7 @@
 package ru.hh.school.users;
 
+import javax.persistence.*;
+
 /**
  * ToDo оформите entity
  *
@@ -7,11 +9,23 @@ package ru.hh.school.users;
  *
  * https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#entity
  */
+@Entity
+@Table(name = "users")
 public class User {
 
+  @Id
+  @Column(name = "user_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
+
+  @Column(name = "first_name")
   private String firstName;
+
+  @Column(name = "last_name")
   private String lastName;
+
+  public User() {
+  }
 
   public User(String firstName, String lastName) {
     this.firstName = firstName;
@@ -26,4 +40,19 @@ public class User {
     this.firstName = firstName;
   }
 
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 }
