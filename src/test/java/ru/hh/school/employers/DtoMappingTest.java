333655f0be57f4a32835ee77b81aa9b97d7122cd
Petr Eremeykin <eremeykin@gmail.com>
2019-02-01 12:17:37 +0300

    Fix fifth test: DtoMappingTest

--- src/test/java/ru/hh/school/employers/DtoMappingTest.java
+++ src/test/java/ru/hh/school/employers/DtoMappingTest.java
@@ -2,17 +2,24 @@ package ru.hh.school.employers;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import org.hibernate.query.Query;
+import org.hibernate.transform.Transformers;
 import org.junit.Test;
 
+import java.util.function.Supplier;
+
 public class DtoMappingTest extends EmployerTest {
+
+  private static final String QUERY_STATISTICS =
+        "select new ru.hh.school.employers.StatisticsDto(count(e.id), sum(e.bonusPoints)) from Employer e";
+        // employerCount и employerCount объявлены final, поэтому ResultTransformer не годится
 
   @Test
   public void mapQueryResultToDto() {
     StatisticsDto dto = doInTransaction(() ->
-      // ToDo дополните запрос, чтобы возвращался ru.hh.school.employers.StatisticsDto
       // https://vladmihalcea.com/the-best-way-to-map-a-projection-query-to-a-dto-with-jpa-and-hibernate/
-      getSession().createQuery(
-        "select count(e.id), sum(e.bonusPoints) from Employer e", StatisticsDto.class)
+      getSession().createQuery( QUERY_STATISTICS, StatisticsDto.class)
         .getSingleResult()
     );
 
