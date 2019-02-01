36399207004a72c4cd29e562e66d2e81bc50b7df
Vitali Baranov <vitali.baranov@gmail.com>
2019-02-02 01:02:07 +0300

    9. TableStrategyTest

--- src/main/java/ru/hh/school/inheritance/table/User.java
+++ src/main/java/ru/hh/school/inheritance/table/User.java
@@ -1,12 +1,6 @@
 package ru.hh.school.inheritance.table;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity(name = "UserTable")
 @Table(name = "user_inheritance_table")
@@ -15,6 +9,7 @@ public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
+  @SequenceGenerator(name = "user_id_seq", allocationSize = 1)
   private Integer id;
 
   private String name;
--- src/main/resources/scripts/create_user_inheritance_table.sql
+++ src/main/resources/scripts/create_user_inheritance_table.sql
@@ -1,10 +1,18 @@
 create sequence user_id_seq;
 
 create table user_inheritance_table (
+    id int not null default nextval('user_id_seq') primary key,
+    name varchar(50) not null
 );
 
 create table employer_inheritance_table (
+    id int not null default nextval('user_id_seq') primary key,
+    name varchar(50) not null,
+    company varchar(50) not null
 );
 
 create table applicant_inheritance_table (
+    id int not null default nextval('user_id_seq') primary key,
+    name varchar(50) not null,
+    position varchar(50) not null
 );
