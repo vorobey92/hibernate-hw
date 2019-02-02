ba85c56dcffc9e6abd66b09925852f5a3b25b1d3
Afftobus <filippoff@mail.ru>
2019-02-02 14:50:43 +0300

    TableStrategyTest

--- src/main/resources/scripts/create_user_inheritance_table.sql
+++ src/main/resources/scripts/create_user_inheritance_table.sql
@@ -1,10 +1,20 @@
+-- очень сомневаюсь по поводу того, нужны ли NOT NULL и если да, то где
+
 create sequence user_id_seq;
 
 create table user_inheritance_table (
+    id INTEGER NOT NULL DEFAULT nextval('user_id_seq') PRIMARY KEY,
+    name VARCHAR NOT NULL
 );
 
 create table employer_inheritance_table (
+    id INTEGER NOT NULL DEFAULT nextval('user_id_seq') PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    company VARCHAR NOT NULL
 );
 
 create table applicant_inheritance_table (
+    id INTEGER NOT NULL DEFAULT nextval('user_id_seq') PRIMARY KEY,
+    NAME VARCHAR NOT NULL,
+    position VARCHAR NOT NULL
 );
