30aab10a6082305c45cc6c6f7b9802469514f8c9
Afftobus <filippoff@mail.ru>
2019-02-02 14:31:57 +0300

    SingeTableTest

--- src/main/resources/scripts/create_user_inheritance_single.sql
+++ src/main/resources/scripts/create_user_inheritance_single.sql
@@ -1,2 +1,9 @@
+-- минимальная необходимая структура, которая позволяет тестам пройти успешно.
+
 create table user_inheritance_single (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    company VARCHAR,
+    position VARCHAR,
+    DTYPE VARCHAR
 );
