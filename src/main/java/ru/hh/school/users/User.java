874ff573e2aba8038d5bd25c7926037ceef09c25
Afftobus <filippoff@mail.ru>
2019-02-02 13:24:39 +0300

    Basic Operations

--- src/main/java/ru/hh/school/users/User.java
+++ src/main/java/ru/hh/school/users/User.java
@@ -1,29 +1,56 @@
 package ru.hh.school.users;
 
+import javax.persistence.*;
+
 /**
  * ToDo оформите entity
  *
  * @see scripts/create_users.sql
- *
+ * <p>
  * https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#entity
  */
+@Entity
+@Table(name = "users")
 public class User {
 
-  private Integer id;
-  private String firstName;
-  private String lastName;
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
 
-  public User(String firstName, String lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-  public Integer getId() {
-    return id;
-  }
+    // геттеры для полей
+    public Integer getId() {
+        return id;
+    }
 
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // сеттеры
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
 }
