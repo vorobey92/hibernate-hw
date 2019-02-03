a0600a10a55aa9c6e7c22cdb668ce4c69791b267
Konstantin_Rogozhinski <rogozhinskiks@gmail.com>
2019-02-04 01:36:58 +0300

    TableStrategyTest

--- src/main/resources/scripts/create_user_inheritance_table.sql
+++ src/main/resources/scripts/create_user_inheritance_table.sql
@@ -1,10 +1,18 @@
 create sequence user_id_seq;
 
 create table user_inheritance_table (
+    id serial primary key,
+    name varchar
 );
 
 create table employer_inheritance_table (
+    id serial primary key,
+    name varchar,
+    company varchar
 );
 
 create table applicant_inheritance_table (
+    id serial primary key,
+    name varchar,
+    position varchar
 );
