7e396acfd27eb4e9e6d7fe14359291a27d3e5adb
Konstantin_Rogozhinski <rogozhinskiks@gmail.com>
2019-02-04 00:23:59 +0300

    SingleTableTest

--- src/main/resources/scripts/create_user_inheritance_single.sql
+++ src/main/resources/scripts/create_user_inheritance_single.sql
@@ -1,2 +1,9 @@
+drop table if exists user_inheritance_single;
+
 create table user_inheritance_single (
+    dtype varchar,
+    id int,
+    name varchar,
+    company varchar,
+    position varchar
 );
