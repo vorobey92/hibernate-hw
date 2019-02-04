ee9460cbc9fc2228ccc1a0090df44822d5bfc4d9
TimaVolgin <tim.volgin@mail.ru>
2019-02-04 23:33:45 +0300

    01. Test BasicOperations

--- src/main/java/ru/hh/school/users/User.java
+++ src/main/java/ru/hh/school/users/User.java
@@ -1,5 +1,7 @@
 package ru.hh.school.users;
 
+import javax.persistence.*;
+
 /**
  * ToDo оформите entity
  *
@@ -7,11 +9,21 @@ package ru.hh.school.users;
  *
  * https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#entity
  */
+
+@Entity
+@Table(name = "users")
 public class User {
 
+  @Id
+  @Column(name = "user_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
+  @Column(name = "first_name")
   private String firstName;
+  @Column(name = "last_name")
   private String lastName;
+
+  public User() {}
 
   public User(String firstName, String lastName) {
     this.firstName = firstName;
@@ -22,8 +34,23 @@ public class User {
     return id;
   }
 
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
 
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 }
