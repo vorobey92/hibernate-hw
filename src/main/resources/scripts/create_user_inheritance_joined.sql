006a4ec434c17819af1bcc78fd9a3e8b1c1e7eb8
TimaVolgin <tim.volgin@mail.ru>
2019-02-04 23:57:11 +0300

    08. Test Joined

--- src/main/resources/scripts/create_user_inheritance_joined.sql
+++ src/main/resources/scripts/create_user_inheritance_joined.sql
@@ -1,9 +1,15 @@
 
 create table user_inheritance_joined (
+  id serial primary key,
+  name varchar(128) not null
 );
 
 create table employer_inheritance_joined (
+  id serial primary key,
+  company varchar(128) not null
 );
 
 create table applicant_inheritance_joined (
+  id serial primary key,
+  position varchar(128) not null
 );
